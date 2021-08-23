0f3bb90d541b5280b31a245e101f2a146cb9c53b
Vijay <vjdubey318@gmail.com>
2021-08-23 16:46:47 +0530

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'employee-management-system-ui';
+  title = 'Employee Management System';
 }
